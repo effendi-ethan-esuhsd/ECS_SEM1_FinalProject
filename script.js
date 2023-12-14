3b448fbe5b0d363dbc42d4d49785b4a68e4b913b
Eth Eff <etheff@Eths-MBP.attlocal.net>
2023-12-13 22:13:38 -0800

    dec13_10:13PM

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-var input = document.getElementById("userInput");
-
-function makeOutputElement(event){
-	if(event.key == "Enter"){
-		const newp = document.createElement("p");
-		const newContent = document.createTextNode("Hi there")
-		newp.appendChild(newContent)
-		const currStuff = document.getElementById("userInput")
-		document.body.insertAfter(currStuff, newp)
-		
-	}
-}
-
-input.addEventlistener("keypress", produceOutput(event));
+window.addEventListener("keypress", function(e){
+   if(e.key == "Enter"){
+       //Produce Requested Content/Action
+       const currentDiv = document.getElementById("currentDiv")
+       const currentInput = document.getElementById("currentInput")
+       const currentInputValue = String(currentInput.value)
+       console.log(currentInputValue)
+       if (currentInputValue === "about age") {
+           const newh1 = document.createElement("h1")
+           const newText = document.createTextNode("14")
+           newh1.append(newText)
+           currentDiv.append(newText)
+       } else if (currentInputValue === "world") {
+       }
+       //Create New Input
+       const newDiv = document.createElement("div")
+       const newInput = document.createElement("input")
+       newDiv.append(newInput)
+       document.body.insertBefore(newDiv, currentDiv)
+       //Change IDs
+       currentDiv.id = "done"
+       newDiv.id = "currentDiv"
+       currentInput.id = "done"
+       newInput.id = "currentInput"
+   }
+})
